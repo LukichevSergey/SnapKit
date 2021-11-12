f99f11472a7fe5cdec067165285ee15c001b0e7e
LukichevSergey <Lukichev_s@mail.ru>
2021-11-12 08:44:26 +0700

    Верстка экрана

--- SnapKitTest/ViewController.swift
+++ SnapKitTest/ViewController.swift
@@ -48,13 +48,48 @@ class ViewController: UIViewController {
         return stack
     }()
     
+    let button13 = Button(title: "7", color: .darkGray, textAlignment: .center).settingButton()
+    let button14 = Button(title: "8", color: .darkGray, textAlignment: .center).settingButton()
+    let button15 = Button(title: "9", color: .darkGray, textAlignment: .center).settingButton()
+    let button16 = Button(title: "*", color: .orange, textAlignment: .center).settingButton()
+    
+    lazy var stack4: UIStackView = {
+        let stack          = UIStackView(arrangedSubviews: [button13, button14, button15, button16])
+        stack.distribution = .fillProportionally
+        stack.spacing      = 10
+        stack.axis         = .horizontal
+        return stack
+    }()
+    
+    let button17 = Button(title: "AC", color: .lightGray, textAlignment: .center).settingButton()
+    let button18 = Button(title: "+/-", color: .lightGray, textAlignment: .center).settingButton()
+    let button19 = Button(title: "%", color: .lightGray, textAlignment: .center).settingButton()
+    let button20 = Button(title: "/", color: .orange, textAlignment: .center).settingButton()
+    
+    lazy var stack5: UIStackView = {
+        let stack          = UIStackView(arrangedSubviews: [button17, button18, button19, button20])
+        stack.distribution = .fillProportionally
+        stack.spacing      = 10
+        stack.axis         = .horizontal
+        return stack
+    }()
+    
     
     lazy var mainStack: UIStackView = {
-        let stack          = UIStackView(arrangedSubviews: [stack3, stack2, stack])
+        let stack          = UIStackView(arrangedSubviews: [stack5, stack4, stack3, stack2, stack])
         stack.distribution = .fillProportionally
         stack.spacing      = 10
         stack.axis         = .vertical
         return stack
+    }()
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.textAlignment = .right
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 65)
+        return label
     }()
 
     override func viewDidLoad() {
@@ -84,6 +119,18 @@ class ViewController: UIViewController {
         
         mainStack.addSubview(stack3)
         stack3.snp.makeConstraints { make in
+            make.right.left.equalTo(mainStack)
+            make.centerX.equalTo(mainStack)
+        }
+        
+        mainStack.addSubview(stack4)
+        stack4.snp.makeConstraints { make in
+            make.right.left.equalTo(mainStack)
+            make.centerX.equalTo(mainStack)
+        }
+        
+        mainStack.addSubview(stack5)
+        stack5.snp.makeConstraints { make in
             make.right.left.equalTo(mainStack)
             make.centerX.equalTo(mainStack)
         }
@@ -143,6 +190,54 @@ class ViewController: UIViewController {
         stack3.addSubview(button12)
         button12.snp.makeConstraints { make in
             make.height.equalTo(self.button12.snp.width)
+        }
+        
+        stack4.addSubview(button13)
+        button13.snp.makeConstraints { make in
+            make.height.equalTo(self.button13.snp.width)
+        }
+        
+        stack4.addSubview(button14)
+        button14.snp.makeConstraints { make in
+            make.height.equalTo(self.button14.snp.width)
+        }
+        
+        stack4.addSubview(button15)
+        button15.snp.makeConstraints { make in
+            make.height.equalTo(self.button15.snp.width)
+        }
+        
+        stack4.addSubview(button16)
+        button16.snp.makeConstraints { make in
+            make.height.equalTo(self.button16.snp.width)
+        }
+        
+        stack5.addSubview(button17)
+        button17.snp.makeConstraints { make in
+            make.height.equalTo(self.button17.snp.width)
+        }
+        
+        stack5.addSubview(button18)
+        button18.snp.makeConstraints { make in
+            make.height.equalTo(self.button18.snp.width)
+        }
+        
+        stack5.addSubview(button19)
+        button19.snp.makeConstraints { make in
+            make.height.equalTo(self.button19.snp.width)
+        }
+        
+        stack5.addSubview(button20)
+        button20.snp.makeConstraints { make in
+            make.height.equalTo(self.button20.snp.width)
+        }
+        
+        self.view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.height.equalTo(130)
+            make.right.left.equalTo(self.view).inset(10)
+            make.centerX.equalTo(self.view)
+            make.bottom.equalTo(mainStack.snp.top)
         }
     }
     
